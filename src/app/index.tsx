f0a6b08ffe53fa7cf7521eb8eb856e4f2a7daabd
dorincreciun <creciundorin25@gmail.com>
2025-11-10 15:24:01 +0200

    feat(prettier): add tailwind class order

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,19 @@
-import {createRoot} from 'react-dom/client'
-import {BrowserRouter} from "react-router";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 
-import '../shared/config/index.css'
-import {Routing} from "@app/providers/routing/Routing.tsx";
+import "../shared/config/index.css";
+import { Routing } from "@app/providers/routing/Routing.tsx";
 
 const container = document.getElementById("root");
 
 if (!container) {
-    throw new Error("Root container with id 'root' not found");
+  throw new Error("Root container with id 'root' not found");
 }
 
 const root = createRoot(container);
 
 root.render(
-    <BrowserRouter>
-        <Routing />
-    </BrowserRouter>
+  <BrowserRouter>
+    <Routing />
+  </BrowserRouter>,
 );
